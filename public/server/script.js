3251ab9d2fbe290d33d0675a1858ed36d097d626
Nayali <nayalithalaramba@gmail.com>
2024-12-10 09:17:39 +0530

    Added inserver  files for Gearheads project

---
+++ public/server/script.js
@@ -0,0 +1,21 @@
+console.log("Gearheads website loaded");
+
+// import mongoose from 'mongoose';
+
+// const carSchema = new mongoose.Schema({
+//     name: String,
+//     brand: String,
+//     year: Number
+// });
+
+// const Car = mongoose.model('Car', carSchema);
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const list = document.getElementById('myList');
+//     for (let i = 1; i <= 49; i++) {
+//         const li = document.createElement('li');
+//         li.textContent = 'Item ' + i;
+//         list.appendChild(li);
+//     }
+// });
+
